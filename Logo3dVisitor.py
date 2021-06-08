82df621f335323bc577e9e85a40cae43064932ea
aleibz98 <alejandro.alarcon.t@est.fib.upc.edu>
2021-06-09 00:26:14 +0200

    millores visitor

--- Logo3dVisitor.py
+++ Logo3dVisitor.py
@@ -56,30 +56,47 @@ class Logo3dVisitor(ParseTreeVisitor):
 
     # Visit a parse tree produced by Logo3dParser#forloop.
     def visitForloop(self, ctx: Logo3dParser.ForloopContext):
+        iterador = self.visit(ctx.VARIABLE().getText())
+        val_entrada = self.visit(ctx.expresio(0))
+        val_salida = self.visit(ctx.expresio(1))
+        sentencias = [sent for sent in self.visit(ctx.sentencia())]
+
+        for i in range(val_entrada, val_salida):
+            self.vars[iterador] = val_entrada
+            # Ejecutar sentencias
+
         return self.visitChildren(ctx)
 
     # Visit a parse tree produced by Logo3dParser#lectura.
     def visitLectura(self, ctx: Logo3dParser.LecturaContext):
+        ids = [id for id in self.visit(ctx.expresio())]
+        for id in ids:
+            self.vars[id] = input(id)
+
         return self.visitChildren(ctx)
 
     # Visit a parse tree produced by Logo3dParser#escriptura.
     def visitEscriptura(self, ctx: Logo3dParser.EscripturaContext):
+        expresions = [expresio for expresio in self.visit(ctx.expresio())]
+        print(expresions)
+
         return self.visitChildren(ctx)
 
     # Visit a parse tree produced by Logo3dParser#declaraciof.
     def visitDeclaraciof(self, ctx: Logo3dParser.DeclaraciofContext):
+        funcio = self.visit(ctx.funcio())
+        sentencies = [sentencia for sentencia in self.visit(ctx.sentencia())]
+
         return self.visitChildren(ctx)
 
     # Visit a parse tree produced by Logo3dParser#invocaciof.
     def visitInvocaciof(self, ctx: Logo3dParser.InvocaciofContext):
         return self.visitChildren(ctx)
 
-    # Visit a parse tree produced by Logo3dParser#comentari.
-    def visitComentari(self, ctx: Logo3dParser.ComentariContext):
-        return self.visitChildren(ctx)
-
     # Visit a parse tree produced by Logo3dParser#funcio.
     def visitFuncio(self, ctx: Logo3dParser.FuncioContext):
+        identificador_funcio = self.VARIABLE().getText()
+        parametres = [arg for arg in self.visit(ctx.expresio())]
         return self.visitChildren(ctx)
 
     # Visit a parse tree produced by Logo3dParser#expresio.
